5a9df2b3496ea2159735197b8cd9ec4f9981bd2f
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-17 14:16:58 +0530

    area added

---
+++ src/area.java
@@ -0,0 +1,9 @@
+public class dataType {
+    public static void main(String[] args) {
+        double x = 39;
+        double    y = 3;
+        System.out.println(x/y);
+        System.out.println(x*y);
+
+    }
+}
