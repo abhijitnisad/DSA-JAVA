9b6e83140fabcd9964cec7f3162c9c8cd366ca0d
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-20 21:12:47 +0530

    take input and sum

--- src/TakeInput.java
+++ src/TakeInput.java
@@ -6,11 +6,20 @@ public class TakeInput {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your number:");
         int num_1 = sc.nextInt();
-        System.out.print("your lucky number:" + num_1);
-        System.out.println("Enter your name:");
+        System.out.println("your lucky number:" + num_1);
+        System.out.print("Enter your name:");
         String name = sc.next();
         System.out.println("your  name is " + name);
 
 
+        //take 2 number input from user and add them
+
+        System.out.print("Enter 1st number:");
+        int num_2 = sc.nextInt();
+
+        System.out.print("Enter your 2nd number:");
+        int num_3 = sc.nextInt();
+
+        System.out.println("Sum:" + (num_2 + num_3));
     }
 }
