f2696051da08ec8cc7b431cede74f4821253a3d1
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-21 21:17:38 +0530

    char data type added

--- src/TakeInput.java
+++ src/TakeInput.java
@@ -4,25 +4,30 @@ public class TakeInput {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter your number:");
-//        int num_1 = sc.nextInt();
-//        System.out.println("your lucky number:" + num_1);
-//        System.out.print("Enter your name:");
-//        String name = sc.next();
-//        System.out.println("your  name is " + name);
-//
-//
-//        //take 2 number input from user and add them
-//
-//        System.out.print("Enter 1st number:");
-//        int num_2 = sc.nextInt();
-//
-//        System.out.print("Enter your 2nd number:");
-//        int num_3 = sc.nextInt();
-//
-//        System.out.println("Sum:" + (num_2 + num_3));
 
+      /*
+        System.out.print("Enter your number:");
+        int num_1 = sc.nextInt();
+        System.out.println("your lucky number:" + num_1);
+        System.out.print("Enter your name:");
+        String name = sc.next();
+        System.out.println("your  name is " + name);
+
+
+        //take 2 number input from user and add them
+
+        System.out.print("Enter 1st number:");
+        int num_2 = sc.nextInt();
+
+        System.out.print("Enter your 2nd number:");
+        int num_3 = sc.nextInt();
+
+        System.out.println("Sum:" + (num_2 + num_3));
+
+       */
+//##############################################################/
         // Simple interest
+
 
         System.out.print("Enter P:");
         double p = sc.nextDouble();
---
+++ src/charDataType.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class charDataType {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a letter :");
+        char a = sc.next().charAt(0);
+
+        System.out.print("Enter 2nd letter :");
+        char b = sc.next().charAt(1);
+
+        System.out.println("Your letter is " + a + " and " +b );
+
+
+
+
+
+
+    }
+
+
+
+}
+
