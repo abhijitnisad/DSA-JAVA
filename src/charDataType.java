b4c6c4f22815d6b66a4d773c5eb0808cb28ca4f5
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-22 12:05:12 +0530

    ASCI value added

--- src/charDataType.java
+++ src/charDataType.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class charDataType {
     public static void main(String[] args) {
+       /*
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter a letter :");
         char a = sc.next().charAt(0);
 
@@ -11,6 +11,32 @@ public class charDataType {
         char b = sc.next().charAt(1);
 
         System.out.println("Your letter is " + a + " and " +b );
+
+
+        char ch = 'f';
+        char ch1 = '+';
+        char ch2 = '$';
+        System.out.println(ch);
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+        */
+
+// ASCI value
+
+        char c1 = 'a';
+        int c1_int = c1;     // Implicit Typecasting
+        System.out.println( "c1 = " + c1_int);
+
+        char c2 = 'D';
+        int c2_int= (int)c2; //Explicit typecasting
+
+        System.out.println(c2_int);
+
+        char c3 = 'a';
+        System.out.println(c3 + 2);
+
+
 
 
 
