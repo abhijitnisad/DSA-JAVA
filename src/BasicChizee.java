9d30c4d5035af00649ef095d55a51286c8dcb6f2
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-22 14:06:25 +0530

    Increment and Decrement operator added

---
+++ src/BasicChizee.java
@@ -0,0 +1,32 @@
+public class BasicChizee
+{
+    public static void main(String[] args) {
+       /*
+        int a= 5;
+        System.out.println("a= " + a);
+        a++;
+        System.out.println("a= " + a);
+
+        int b= 10;
+        System.out.println("B=" + b);
+        b++;
+        System.out.println("B=" + b);
+
+        */
+
+//        ############################### Increment and Decrement operator #######################################
+//        ++x, x++ , --x , x--
+
+
+        int x = 5;
+//        System.out.println(++x); // first increase then print
+//        System.out.println(x++); // first print then increase
+//        System.out.println(x--); // first print then decrease
+//        System.out.println(--x); // first decrease then print
+
+        int y = x++; // y= 5 but here x = 6
+        System.out.println(x +" " + y);
+
+
+    }
+}
