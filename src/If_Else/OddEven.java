b592718a0a3bb82c6328c3234010f9a21a590411
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-23 23:23:50 +0530

    Odd even and divisibility check added

---
+++ src/If_Else/OddEven.java
@@ -0,0 +1,36 @@
+package If_Else;
+
+import java.util.Scanner;
+
+public class OddEven  {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        /*
+
+        //Odd & Even number
+
+        System.out.print("Enter your number: ");
+        int n = sc.nextInt();
+        if (n%2 == 0){
+            System.out.println("Your number " + n  + " which is even");
+        }else {
+            System.out.println("Your number " + n + "  is odd");
+        }
+         */
+
+        //#####################################################################################
+
+        // Check divisibility
+
+
+        System.out.print("Enter your number:");
+        int x = sc.nextInt();
+        if (x % 5 == 0) {
+            System.out.println("Your number is divisible by 5 ");
+        }else {
+            System.out.println("Not divisible by 5 and remainder is " + (x % 5));
+        }
+    }
+}
