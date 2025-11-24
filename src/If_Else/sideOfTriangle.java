670979a0f16fa8fdfd9dd55fc2fd99500f309251
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-24 15:24:36 +0530

    check triangle validit

---
+++ src/If_Else/sideOfTriangle.java
@@ -0,0 +1,24 @@
+package If_Else;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class sideOfTriangle {
+    public static void main(String[] args) {
+        //Ques: Take 3 positive integers input and tell if they can be the sides of a triangle or not.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter 1st number:");
+        int x = sc.nextInt();
+        System.out.print("enter 2nd number:");
+        int y = sc.nextInt();
+        System.out.print("enter 3rd number:");
+        int z = sc.nextInt();
+
+        if((y+z>x) && (x+z>y) && ( x+y>z)){
+            System.out.println("Its triange ");
+        }else {
+            System.out.println("not triange");
+        }
+
+    }
+}
