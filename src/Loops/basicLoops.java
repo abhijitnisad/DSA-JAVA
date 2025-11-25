10d098be670cfe4e63b1af9e859f40b30b56d4ca
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-25 15:59:54 +0530

    basic loop added

---
+++ src/Loops/basicLoops.java
@@ -0,0 +1,18 @@
+package Loops;
+
+public class basicLoops {
+    public static void main(String[] args) {
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Abhijit");
+
+                    //Initializaton  only one time hoti hai.
+                    //but these repeats in loops
+                    //1) 1st condition check
+                    //2) work or sout
+                    //3) then incremant and these loops until the condition true.
+
+        }
+    }
+
+
+}
