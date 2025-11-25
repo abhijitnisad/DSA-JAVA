e7876df06a5cc6ce40ff0b23e037d041f6857128
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-25 22:56:31 +0530

    basic loops added

--- src/Loops/basicLoops.java
+++ src/Loops/basicLoops.java
@@ -1,7 +1,13 @@
 package Loops;
 
+import java.util.Scanner;
+
 public class basicLoops {
     public static void main(String[] args) {
+
+
+        
+                /*
                 for (int i = 0; i < 3; i++) {
                     System.out.println("Abhijit");
 
@@ -11,7 +17,23 @@ public class basicLoops {
                     //2) work or sout
                     //3) then incremant and these loops until the condition true.
 
-        }
+                }
+                */
+
+                //  for (int i = 0; i <100 ; i++) {
+                //     System.out.print(i + " ");
+                //  }
+
+                //Ques: Print input from user 'n' times. Take 'n';
+
+                Scanner sc  = new Scanner(System.in);
+                int n = sc.nextInt();
+
+                for (int i = 0; i <= n; i++) {
+                    System.out.println("Divya" );
+                }
+
+
     }
 
 
---
+++ src/Loops/input.java
@@ -0,0 +1,9 @@
+// package Loops;
+
+public class input {
+    public static void main(String[] args) {
+        System.out.println("abhijit");
+    }
+}
+
+
