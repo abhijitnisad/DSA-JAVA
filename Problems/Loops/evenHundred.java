cd067cb1c6df2a65435c34d383284c535a6bc190
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-26 00:08:28 +0530

    even number added

---
+++ Problems/Loops/evenHundred.java
@@ -0,0 +1,10 @@
+public class evenHundred {
+    public static void main(String[] args) {
+        //even between 1 to 100;
+        for (int i = 0; i <=100; i=i+2) {
+            // if(i%2==0) System.out.print(i + " ");
+            System.out.print(i + " ");
+            ;
+        }
+    }
+}
--- Problems/Loops/input.java
+++ Problems/Loops/input.java
@@ -1,10 +1,20 @@
 // package Loops;
 
+import java.util.Scanner;
+
 public class input {
     public static void main(String[] args) {
 
         //Print n-times Divya where n is taken from user.
-        System.out.println("hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number:");
+        int n = sc.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print("Divya ");
+        }
+
+
 
     }
 }
