46a2f0822b2a4fae71efbf4831b5cca376e72ec0
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-25 23:04:04 +0530

    Loop file moved

---
+++ Problems/Loops/basicLoops.java
@@ -0,0 +1,40 @@
+// package Loops;
+
+import java.util.Scanner;
+
+public class basicLoops {
+    public static void main(String[] args) {
+
+
+        
+                /*
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Abhijit");
+
+                    //Initializaton  only one time hoti hai.
+                    //but these repeats in loops
+                    //1) 1st condition check
+                    //2) work or sout
+                    //3) then incremant and these loops until the condition true.
+
+                }
+                */
+
+                //  for (int i = 0; i <100 ; i++) {
+                //     System.out.print(i + " ");
+                //  }
+
+                //Ques: Print input from user 'n' times. Take 'n';
+
+                Scanner sc  = new Scanner(System.in);
+                int n = sc.nextInt();
+
+                for (int i = 0; i <= n; i++) {
+                    System.out.println("Divya" );
+                }
+
+
+    }
+
+
+}
---
+++ Problems/Loops/input.java
@@ -0,0 +1,8 @@
+// package Loops;
+
+public class input {
+    public static void main(String[] args) {
+        System.out.println("abhijit");
+    }
+}
+
