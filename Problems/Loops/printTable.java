3d21715c8d6493fc34f0b2b3daca7fd53e298959
abhijitnisad <abhijit.nisad99@gmail.com>
2025-11-26 20:29:27 +0530

    Print table added

---
+++ Problems/Loops/printTable.java
@@ -0,0 +1,22 @@
+public class printTable {
+    public static void main(String[] args) {
+
+        //Table of 17;
+
+        // for (int i = 17; i < 170; i++) {// loop 154 time iterate krega;
+        //  if (i%17 == 0) {
+        //     System.out.print (i + " ");
+        //  }   
+        // }
+
+
+        // for (int i = 17; i <= 170; i=i+17) {
+        //     System.out.print(i + " ");
+        // }
+
+
+        for (int i = 1; i <=10; i++) {
+            System.out.print(i*17 + " ");
+        }
+    }
+}
