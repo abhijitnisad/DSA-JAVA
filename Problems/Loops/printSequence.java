b836a122dabf10b8081aacd73ca41c6c13019957
abhijitnisad <abhijit.nisad99@gmail.com>
2025-12-03 20:45:13 +0530

    print sequence added

---
+++ Problems/Loops/printSequence.java
@@ -0,0 +1,12 @@
+public class printSequence {
+        public static void main(String[] args) {
+            /*
+            HW: Take 'n' as input from user and
+            print the following sequence..
+            n
+            2
+            n-1
+            3
+            */
+        }
+}
