3fa205b8a18e0d4a6733497e261350b446d8927a
abhijitnisad <abhijit.nisad99@gmail.com>
2025-12-04 12:04:28 +0530

    prime and composite number added

---
+++ Problems/Loops/compositeAndPrime.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class compositeAndPrime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number:");
+        int num = sc.nextInt();
+
+        /*
+        for (int i = 2; i <= num-1; i++) {
+            if (num % i == 0) {
+                System.out.print("composite number");
+            }else System.out.println("Prime number");
+            break;
+        }
+        */
+
+        //Another method
+        boolean flag = false;
+        for (int i = 2; i < num; i++) {
+            if(num % i == 0){
+                flag = true;
+                break;
+            }
+        }
+
+            if (num == 1) {
+                System.out.println("Neither prime nor composite number");
+            }else if (flag == true) {
+                System.out.println("Composite number");
+            }else {System.out.println("Prime number");}
+    }
+}
