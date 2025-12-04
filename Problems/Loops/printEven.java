f39aa712e946052cdc4055ed3f982ce2cb88efc0
abhijitnisad <abhijit.nisad99@gmail.com>
2025-12-04 12:28:26 +0530

    print even number

---
+++ Problems/Loops/printEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class printEven {
+    public static void main(String[] args) {
+        /*
+        Ques: Print all even numbers from 1 to 100
+         */
+
+        // System.out.println("Enter number = ");
+        // Scanner sc = new Scanner(System.in);
+
+        for (int i = 1; i <= 100; i++) {
+            if (i%2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
