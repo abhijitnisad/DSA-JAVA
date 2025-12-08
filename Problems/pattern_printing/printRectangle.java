a59c64896c59e5eb9a941d201acac0f36c67b4dc
abhijitnisad <abhijit.nisad99@gmail.com>
2025-12-08 11:21:31 +0530

    print ractangle added

--- Problems/pattern_printing/printRectangle.java
+++ Problems/pattern_printing/printRectangle.java
@@ -1,5 +1,14 @@
 public class printRectangle {
     public static void main(String[] args) {
-        System.out.println("ABhijit");
+    
+        //Print rectangle
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    
+    
     }
 }
